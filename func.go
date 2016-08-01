fbd62587002ba29b84b257a47602bbc6603aca8c
can_pc <1835373375@qq.com>
2016-08-01 10:09:34 +0800

    go函数练习

---
+++ func.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 20
+
+	fmt.Println(sum(a, b))
+	fmt.Println(area(a, b))
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func area(a, b int) int {
+	return a * b
+}
