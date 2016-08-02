021b0eef136c736b22bd089845600879006145d1
can_pc <1835373375@qq.com>
2016-08-02 15:04:52 +0800

    发送http请求

---
+++ http.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	httpGet()
+}
+
+func httpGet() {
+	resp, err := http.Get("https://studygolang.com/static/pkgdoc/index.html#archive")
+	if err != nil {
+		//handle error
+
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		//handle error
+
+	}
+	fmt.Println(string(body))
+
+}
+
+//使用http.Post方法
+func httpPost1() {
+	resp, err := http.Post("http://www.01happy.com/demo/accept.php",
+		"application/x-www-form-urlencoded",
+		strings.NewReader("name=cjb"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
+
+//使用http.PostForm方法
+func httpPost2() {
+	resp, err := http.PostForm("http://www.01happy.com/demo/accept.php",
+		url.Values{"key": {"Value"}, "id": {"123"}})
+
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
+
+//复杂的请求(比如设置头参数，cookie之类的数据)
+func httpDo() {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", "http://www.01happy.com/demo/accept.php", strings.NewReader("name=cjb"))
+	if err != nil {
+		// handle error
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Cookie", "name=anny")
+
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
