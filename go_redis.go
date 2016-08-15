88b7f54bebbd32f73467e67f54738f50820bfedd
can_pc <1835373375@qq.com>
2016-08-15 13:31:24 +0800

    go操作redis

---
+++ go_redis.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+func main() {
+	ope_redis()
+}
+
+func ope_redis() {
+	c, err := redis.Dial("tcp", "127.0.0.1:6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	v, err := c.Do("SET", "name", "red")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(v)
+	v, err = redis.String(c.Do("Get", "name"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(v)
+}
