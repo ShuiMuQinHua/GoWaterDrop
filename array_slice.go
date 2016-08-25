90ab80028e3357f6c29519ef46fc5355a4b06198
can_pc <1835373375@qq.com>
2016-08-25 18:00:09 +0800

    数组与切片

---
+++ array_slice.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr()
+}
+
+func arr() {
+	var arr1 [5]int
+	for i := 0; i < len(arr1); i++ {
+		arr1[i] = i * 2
+	}
+	for i := 0; i < len(arr1); i++ {
+		fmt.Printf("Array at index %d is %d\n", i, arr1[i])
+	}
+}
